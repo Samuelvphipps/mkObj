0699106665c724591e06fbf6606814737c9a7b12
Samuelvphipps <samuel.v.phipps@gmail.com>
2024-06-16 19:13:32 -0500

    errors

---
+++ README.md
@@ -0,0 +1,26 @@
+# Command Line Dictionary App for storing and modifying an object
+
+This is a tool that creates an interface and then runs a CLI app based on the requirements laid out in the `requirements.md` file.
+
+### Commands
+ - ADD: Adds a key value pair
+ - REMOVE: Removes a member from specified key
+ - MEMBERS: Returns the collection of strings for given key
+ - KEYS: Returns all the keys in the dictionary
+ - REMOVEALL: Removes all members for a key and removes the key from the dictionary
+ - CLEAR: Removes all keys and members, empties the dictionary
+ - KEYEXISTS: Returns a boolean if the given key is in the dictionary
+ - MEMBEREXISTS: Returns a boolean for whether a member exists within a key.
+ - ALLMEMBERS: Returns all the members of the dictionary
+ - ITEMS: Returns all keys in the dictionary and all their members
+
+## How Run on your machine:
+ - Clone this repo
+ - Open in your IDE or open directory in your terminal 
+ - Run `npm install` in your terminal
+ - Run `npm run dictionary` in your terminal and have fun!
+ - Run `-h` or `--help` to see more detailed documentation
+ - Run `EXIT` to shut down the app
+
+## Support
+If you have suggestions or issues, please email me at **samuel.v.phipps@gmail.com** or contact me via [LinkedIn](https://www.linkedin.com/in/sam-v-phipps/)
--- src/index.ts
+++ src/index.ts
@@ -8,51 +8,62 @@ let dictionary = new Dictionary();
 
 
 program
-  .name('multi-value-dictionary')
-  .description('Multi-Value Dictionary CLI')
-  .exitOverride()
+    .name('multi-value-dictionary')
+    .description('Multi-Value Dictionary CLI')
+    .showSuggestionAfterError(true)
+    .configureOutput({
+        outputError: (str, write) => write(errorColor(str))
+    })
+    .exitOverride()
 
 program 
     .command('ADD <key> <member>')
+    .description('Add a member to a collection for a given key')
     .action((key, member) => {
         dictionary.add(key, member);
     })
     
 program
-  .command('KEYS')
-  .action(() => {
+    .command('KEYS')
+    .description('Get all the keys in the dictionary')
+    .action(() => {
     dictionary.keys().forEach((key, i) => console.log(`${i + 1}) ${key}`)) 
-  })
+    })
 
 program
-  .command('MEMBERS <key>')
-  .action((key) => {
-    const members: string[] = dictionary.members(key);
-    if (members) {
-      members.forEach((member, i) => console.log(`${i + 1}) ${member}`));
-    }
-  })
+    .command('MEMBERS <key>')
+    .description('Get all members in a collection for a given key')
+    .action((key) => {
+        const members: string[] = dictionary.members(key);
+        if (members) {
+            members.forEach((member, i) => console.log(`${i + 1}) ${member}`));
+        }
+    })
 
 program
     .command('REMOVE <key> <member>')
+    .description('Remove specified member from a key')
     .action((key, member) => {
         dictionary.remove(key, member);
     })
 
 program
     .command('REMOVEALL <key>')
+    .description('Remove all members from a key')
     .action((key) => {
         dictionary.removeAll(key);
     })
 
 program
     .command('CLEAR')
+    .description('Remove all keys and members')
     .action(() => {
         dictionary.clear();
     })
 
 program
     .command('KEYEXISTS <key>')
+    .description('Check if key exists')
     .action((key) => {
         console.log(dictionary.keyExists(key));
     })
@@ -60,12 +71,14 @@ program
 program
 // todo: handle case where key does not exist and a member arg is still added
     .command('MEMBEREXISTS <key> <member>')
+    .description('Check if member exists for a key')
     .action((key, member) => {
         console.log(dictionary.memberExists(key, member));
     })
 
 program
     .command('ALLMEMBERS')
+    .description('Get all members in the dictionary')
     .action(() => {
         let members = dictionary.allMembers();
         members.length > 0 ? members.forEach((member, i) => console.log(`${i + 1}) ${member}`)) 
@@ -75,6 +88,7 @@ program
 
 program
     .command('ITEMS')
+    .description('Get all keys and members in the dictionary')
     .action(() => {
         console.log(dictionary.items());
     })
@@ -88,15 +102,18 @@ const rl = readline.createInterface({
 
 function handleInput(input: string) {
 
-    if (input.trim() === 'exit') {
+    if (input.trim() === 'EXIT') {
         rl.close();
         return;
       }
-    
       try {
-        program.parse(input.trim().split(' '), { from: 'user' });
-      } catch (error: any) {
-        console.error('Invalid command');
+        if(input.trim() == '-h' || input.trim() == '--help' || input.trim() == 'help') {
+            program.outputHelp()
+        } else {
+            program.parse(input.trim().split(' '), { from: 'user' })
+        }
+      } catch {
+        console.error('Try again, use -h command for assistance');
       }
 
     rl.prompt();
@@ -106,33 +123,7 @@ rl.on('line', handleInput);
 
 rl.prompt();
 
-
-
-
-
-
-
-
-
-
-
-// const [command, ...args] = input.split(' ');
-
-//     switch (command) {
-//         case 'ADD':
-//             const [key, member] = args;
-//             args.length !== 2 ? console.log(') ERROR, invalid number of arguments.') :
-//             dictionary.add(key, member);
-//             break;
-//         case 'KEYS':
-//             console.log(dictionary.keys());
-//             break;
-//         case 'HELP':
-//             console.log('Commands: ADD, KEYS, HELP');
-//             break;
-//         case 'MEMBERS':
-            
-//         default:
-//             console.log('Invalid command, please try again. To see a list of commands, type HELP.');
-//             break;
-//     }
+function errorColor(error: string) {
+    // Add ANSI escape codes to display text in red.
+    return `\x1b[31m${error}\x1b[0m`;
+  }
